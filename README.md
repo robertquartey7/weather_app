81f282bbbc62b405cb4eb3fb7e68be21a630d857
Robert Quartey <37972474+robertquartey7@users.noreply.github.com>
2022-03-08 01:14:17 -0500

    .

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ as input from the user and then displays the current
 weather condition of the city and town entered.
 
 
-
+# home page
 ![home page](./assets/home.png)
+
+# result page
+## this page show the results of the input from the user
+
+![results page](./assets/results.png)
--- app.js
+++ app.js
@@ -4,23 +4,60 @@ const https = require("https");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+//setting a view engine
+app.set('view engine', 'ejs')
+
+//using the static files
+app.use(express.static('public'))
+
+
+
+//rendering the index page if we go to the home page
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render('index')
 });
 
+
+
+//handling post submittions
 app.post("/", function (req, res) {
   const query = req.body.city;
   const units = "imperial";
   const apikey = "b42e4e533c8243abb98bff069cfe710b";
   url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apikey}&units=${units}`;
 
+  //fetching data from API
   https.get(url, (response) => {
-    response.on("data", (data) => {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      console.log(`${temp} ${weatherDescription}`);
-    });
+
+    //handling the error if the api request is not successful
+    if(response.statusCode!=200){
+     const errorMsg = 'city not found please try again'
+
+     res.render('error', {errorMsg:errorMsg})
+
+    }else{
+      
+      response.on("data", (data) => {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon
+        const imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+  
+      
+        const content = {
+          query:query,
+          temp :temp,
+          weatherDescription: weatherDescription,
+          imgUrl:imgUrl,
+        }
+  
+        res.render('result', content)
+  
+  
+      });
+    }
+ 
   });
 });
 
